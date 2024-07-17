7c2b463ad996de95e446600eef385549de604766
Pooranan11 <pooranan.pro@gmail.com>
2024-07-17 14:27:24 -0400

    premier commit

---
+++ PriceObserver.java
@@ -0,0 +1,7 @@
+// PriceObserver.java
+public class PriceObserver implements OrderObserver {
+    @Override
+    public void update(Order order) {
+        // Méthode à implémenter
+    }
+}
---
+++ QuantityObserver.java
@@ -0,0 +1,7 @@
+// QuantityObserver.java
+public class QuantityObserver implements OrderObserver {
+    @Override
+    public void update(Order order) {
+        // Méthode à implémenter
+    }
+}
